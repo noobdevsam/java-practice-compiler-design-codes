f0da870452a18dfe016d1e2bc6782addc214f96a
noobdevsam <mdshoykotislam05@gmail.com>
2026-02-19 18:42:56 +0600

    add HelloWorld class with basic structure and comments

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+// This is a single-line comment
+public class HelloWorld {
+    /*
+        This is a
+        multi-line comment
+    */
+    static void main(String[] args) {
+        System.out.println("Hi there."); // This is a single line comment
+    }
+}
+// This is a test document
