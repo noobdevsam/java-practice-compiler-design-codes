2579f9f96996002f09e2c86ee3d977fe784a682c
noobdevsam <mdshoykotislam05@gmail.com>
2026-02-21 16:54:07 +0600

    add HybridAutomaton class with initial state, accept states, and transition mapping

--- JHybridFiniteAutomata.java
+++ JHybridFiniteAutomata.java
@@ -1,3 +1,17 @@
 void main() {
-    
+
+}
+
+class HybridAutomaton {
+    private final String initialState;
+    private final Set<String> acceptStates;
+
+    // Map: FromState -> (InputSymbol -> Set of  ToStates)
+    private final Map<String, Map<Character, Set<String>>> transitions;
+
+    public HybridAutomaton(String initialState, Set<String> acceptStates) {
+        this.initialState = initialState;
+        this.acceptStates = acceptStates;
+        this.transitions = new HashMap<>();
+    }
 }
